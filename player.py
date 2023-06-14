ca1d1b856360c6000c2ae262d63fbe4327c99f74
gomes cabral <phgcabral@hotmail.com>
2023-06-14 13:31:55 +0200

    create the classes and the turtle object to move forward

---
+++ player.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.shape('turtle')
+        self.color('black')
+        self.goto(STARTING_POSITION)
+        self.left(90)
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
